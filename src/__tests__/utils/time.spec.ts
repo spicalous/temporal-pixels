6e2bfe38d1694bc28ca5cc946971188491c55862
spicalous <benlertlumprasertkul@gmail.com>
2025-08-13 00:13:20 +0100

    fix tests

--- src/__tests__/utils/time.spec.ts
+++ src/__tests__/utils/time.spec.ts
@@ -1,13 +1,33 @@
 import { describe, it, expect } from 'vitest';
 
-import { getElapsedSecondsOfDay } from '@/utils/time.ts';
+import { getElapsedPercentageOfDay, getElapsedPercentageOfWeek } from '@/utils/time.ts';
 
 describe('utils / time', () => {
 
-  it('getElapsedSecondsOfDay', () => {
-    const mockNow = new Date(2000);
-    const tzOffsetMinutes = mockNow.getTimezoneOffset();
-    const tzOffsetSeconds = tzOffsetMinutes * 60 * -1;
-    expect(getElapsedSecondsOfDay(mockNow)).toEqual(2 + tzOffsetSeconds);
+  const TZ_OFFSET_MINUTES = new Date().getTimezoneOffset();
+  const TZ_OFFSET_MILLIS = TZ_OFFSET_MINUTES * 60 * 1000 * -1;
+  // epoch 0 is thursday so we need 3 days in millis to get to sunday 00:00
+  const MILLIS_FROM_EPOCH_TO_SUNDAY = 1000 * 60 * 60 * 24 * 3;
+
+  it('getElapsedPercentageOfDay 1%', () => {
+    // 1% of a day
+    const mockNow = new Date(864000 - TZ_OFFSET_MILLIS);
+    expect(getElapsedPercentageOfDay(mockNow)).toEqual(0.01);
   });
+
+  it('getElapsedPercentageOfDay 50%', () => {
+    const mockNow = new Date(43200000 - TZ_OFFSET_MILLIS);
+    expect(getElapsedPercentageOfDay(mockNow)).toEqual(0.5);
+  });
+
+  it('getElapsedPercentageOfWeek 1%', () => {
+    const mockNow = new Date((864000 * 7) + MILLIS_FROM_EPOCH_TO_SUNDAY - TZ_OFFSET_MILLIS);
+    expect(getElapsedPercentageOfWeek(mockNow)).toEqual(0.01);
+  });
+
+  it('getElapsedPercentageOfWeek 50%', () => {
+    const mockNow = new Date((43200000 * 7) + MILLIS_FROM_EPOCH_TO_SUNDAY - TZ_OFFSET_MILLIS);
+    expect(getElapsedPercentageOfWeek(mockNow)).toEqual(0.5);
+  });
+
 });
